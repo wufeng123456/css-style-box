81c88de5dc0c0c84c0857a89723b6ea643ad5ee2
wufeng <1563889283@qq.com>
2019-04-09 15:35:12 +0800

    新增横向scroll-list

--- src/components/list/scroll-list.vue
+++ src/components/list/scroll-list.vue
@@ -1,7 +1,15 @@
 <template>
 	<div class="scroll-list">
-		<div class="scroll-list__core">
-			未完待续...
+		<div class="scroll-list__container" :style="'width:' + containerWidth + 'px;'">
+			<div v-if="left !== coreOverflowWidth" class="scroll-list__container__before" @click="before"></div>
+			<div class=" scroll-list__container__content">
+				<div class="scroll-list__container__content__core" :style="'left:' + left + 'px;'">
+					<div class="scroll-list__container__content__core__list" v-for="(item, index) in list" :key="index">
+						{{item.name}}
+					</div>
+				</div>
+			</div>
+			<div v-if="left !== 0" class="scroll-list__container__after" @click="after"></div>
 		</div>
 	</div>
 </template>
@@ -11,20 +19,115 @@ export default {
 	name: 'scroll-list',
 	data () {
 		return {
+			list: [
+				{
+					name: '语文语文语文语文'
+				},
+				{
+					name: '数学'
+				},
+				{
+					name: '英语英语英语英语英语'
+				},
+				{
+					name: '日语日语日语'
+				},
+				{
+					name: '法语法语法语法语'
+				},
+				{
+					name: '意大利炮'
+				}
+			],
+			left: 0
+		}
+	},
+	computed: {
+		containerWidth () {
+			return this.list.length > 3 ? 480 : this.list.length * 120
+		},
+		coreOverflowWidth () {
+			return this.list.length > 4 ? -(this.list.length - 4) * 120 : 0
+		}
+	},
+	methods: {
+		before () {
+			if (this.coreOverflowWidth <= this.left - 120) {
+				this.left = this.left - 120
+			}
+		},
+		after () {
+			if (this.left !== 0) {
+				this.left = this.left + 120
+			}
 		}
 	}
 }
 </script>
 
 <style lang="scss">
+@import '../../assets/color.scss';
 .scroll-list {
 	width: 100%;
 	height: 100%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	&__core {
-		color: red;
+	&__container {
+		position: relative;
+		// width: 480px;
+		height: 60px;
+		&__before {
+			position: absolute;
+			top: 19px;
+			left: -30px;
+			width: 0;
+			height: 0;
+			border-width: 10px 10px 10px 10px;
+			border-style: solid;
+			border-color: transparent $main-color transparent transparent;
+			cursor: pointer;
+		}
+		&__content {
+			position: relative;
+			width: 100%;
+			height: 100%;
+			border: 1px solid $main-color;
+			border-right: none;
+			overflow: hidden;
+			&__core {
+				position: absolute;
+				left: 0;
+				width: 100%;
+				height: 100%;
+				color: $main-color;
+				// overflow: hidden;
+				white-space: nowrap;
+				&__list {
+					display: inline-block;
+					width: 120px;
+					height: 60px;
+					line-height: 60px;
+					border-right: 1px solid $main-color;
+					box-sizing: border-box;
+					overflow: hidden;
+					white-space: nowrap;
+					text-overflow: ellipsis;
+					text-align: center;
+				}
+			}
+		}
+		&__after {
+			position: absolute;
+			top: 19px;
+			right: -30px;
+			width: 0;
+			height: 0;
+			border-width: 10px 10px 10px 10px;
+			border-style: solid;
+			border-color: transparent transparent transparent $main-color;
+			cursor: pointer;
+		}
 	}
 }
 </style>
